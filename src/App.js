5625293d65ce76735cddf33b41d1e851dd34e724
Andrey Mamchenko <atmamont@gmail.com>
2017-06-01 22:35:53 +0300

    Login form base implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA0i1ycBUz_CnQJ5O2EOwmolFHDHYnUxsQ',
+      authDomain: 'rnauth-1ba30.firebaseapp.com',
+      databaseURL: 'https://rnauth-1ba30.firebaseio.com',
+      projectId: 'rnauth-1ba30',
+      storageBucket: 'rnauth-1ba30.appspot.com',
+      messagingSenderId: '1079926124985'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>In app</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
